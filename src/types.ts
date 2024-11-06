060482c1171f5f335888aa98f63ac05b548c4312
Max <maxduma16@gmail.com>
2024-11-06 20:49:03 +0200

    ref User types

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,70 @@
 export interface User {
   id: number;
-  image: string;
   firstName: string;
   lastName: string;
-  birthDate: string;
-  gender: string;
-  email: string;
-  phone: string;
+  maidenName?: string;
   username: string;
-  generalInfo?: string;
-  domain?: string;
-  ip?: string;
-  macAddress?: string;
-  address?: {
+  password: string;
+  email: string;
+  gender: string;
+  birthDate: string;
+  age: number;
+  height: number;
+  weight: number;
+  bloodGroup: string;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  address: {
     address: string;
     city: string;
-    state: string;
+    coordinates: {
+      lat: number;
+      lng: number;
+    };
+    country: string;
     postalCode: string;
+    state: string;
+    stateCode: string;
   };
-  bank?: {
+  bank: {
+    cardExpire: string;
     cardNumber: string;
     cardType: string;
+    currency: string;
+    iban: string;
   };
-  university?: string;
-  company?: {
+  company: {
     name: string;
     department: string;
     title: string;
+    address: {
+      address: string;
+      city: string;
+      coordinates: {
+        lat: number;
+        lng: number;
+      };
+      country: string;
+      postalCode: string;
+      state: string;
+      stateCode: string;
+    };
   };
-  ein?: string;
-  ssn?: string;
+  crypto: {
+    coin: string;
+    network: string;
+    wallet: string;
+  };
+  ein: string;
+  ssn: string;
+  image: string;
+  phone: string;
+  role: string;
+  userAgent: string;
+  ip: string;
+  macAddress: string;
+  university: string;
 }
